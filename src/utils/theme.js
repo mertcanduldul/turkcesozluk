e74a8034f263ab58098e8982b07046b499dcc831
MERT CAN DÜLDÜL <mertcanduldul@gmail.com>
2021-01-03 03:58:30 +0300

    radius ve yeni renkler eklendi

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,14 +1,25 @@
 const space = [];
 
 const radii = {
-    normal: 6,
+    normal: 8,
     full: 9999,
 
 }
 const colors = {
     red: '#E11E3C',
     gray: '#758291',
-
+    //
+    softRed: '#F8F8F8',
+    light: '#E8EAED',
+    birlesikKelimeDark: '#2E494C',
+    birlesikKelimeMedium: '#73A5AA',
+    birlesikKelimeLight: '#E8F0F1',
+    atasozleriDark: '#4F3822',
+    atasozleriMedium: '#BB8E62',
+    atasozleriLight: '#F9F5F1',
+    textDark: '#0A151F',
+    textMedium: '#48515B',
+    textLight: '#758291'
 }
 
 export default {
