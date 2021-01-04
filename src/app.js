75fa806da68a575a4c8ce1097cf861ea4b49a694
MERT CAN DÜLDÜL <mertcanduldul@gmail.com>
2021-01-04 22:38:22 +0300

    headerTitle bölümü sayfalar özgü özelleştirildi

--- src/app.js
+++ src/app.js
@@ -15,15 +15,50 @@ import DetailView from './view/detail'
 import Box from './components/box'
 
 import theme from './utils/theme'
+import Button from './components/button';
+
+import { Left, More } from './components/icons'
 
 const Tab = createBottomTabNavigator();
 const HomeStack = createStackNavigator();
 
 function SearchStack() {
     return (
-        <HomeStack.Navigator headerMode="none">
-            <HomeStack.Screen name="Search" component={SearchView} />
-            <HomeStack.Screen name="Detail" component={DetailView} />
+        <HomeStack.Navigator>
+            <HomeStack.Screen name="Search" component={SearchView}
+                options={() => {
+                    return {
+                        headerShown: false 
+                    }
+                }}
+            />
+            <HomeStack.Screen name="Detail" component={DetailView}
+                options={({ route, navigation }) => {
+                    return {
+                        title: (route.params && route.params.title) || 'Boş',
+                        headerStyle: {
+                            backgroundColor: theme.colors.softRed,
+                            shadowColor: 'transparent',
+                        },
+                        headerLeft: () => (
+                            <Button
+                                height="100%"
+                                px={20}
+                                onPress={() => navigation.navigate('Search')}>
+                                <Left color={theme.colors.textDark} />
+                            </Button>
+                        ),
+                        headerRight: () => (
+                            <Button
+                                height="100%"
+                                px={20}
+                                onPress={() => navigation.navigate('Search')}>
+                                <More color={theme.colors.textDark} />
+                            </Button>
+                        ),
+                    }
+                }}
+            />
         </HomeStack.Navigator>
     );
 }
