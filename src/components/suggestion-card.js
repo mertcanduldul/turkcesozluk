1dcecfd6409d400902a092ec2858f96912e6de18
MERT CAN DÜLDÜL <mertcanduldul@gmail.com>
2021-01-05 22:00:36 +0300

    skeleton yapısı adapte edildi

--- src/components/suggestion-card.js
+++ src/components/suggestion-card.js
@@ -2,17 +2,24 @@ import React from 'react'
 import Text from './text'
 import { CardContainer, CardSummary, CardTitle } from './card'
 import Box from './box'
+import { LoaderText } from './LoaderText'
 
 export default function SuggestionCard({ data, onPress, title, ...props }) {
     return (
         <Box {...props}>
             <Text color="textLight">{title}</Text>
-
             <CardContainer mt={10} onPress={onPress}>
-
-                <CardTitle>{data?.madde}</CardTitle>
-                <CardSummary>{data?.anlam}</CardSummary>
-
+                {data ? (
+                    <>
+                        <CardTitle>{data?.madde}</CardTitle>
+                        <CardSummary>{data?.anlam}</CardSummary>
+                    </>
+                ) : (
+                        <>
+                            <LoaderText />
+                            <LoaderText width={200} mt={10} />
+                        </>
+                    )}
             </CardContainer>
         </Box>
     )
